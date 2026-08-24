a1282f40d9292789d50c27f99e08613f76407266
Muhammad Yunus <myunus_info@yahoo.com>
2026-08-25 01:29:34 +0300

    Fix Payment Confirm Route

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -5,7 +5,7 @@ import { Role } from '../../../../generated/prisma/enums';
 
 const router = Router();
 
-router.post('/', paymentController.confirmPayment);
+router.post('/confirm', paymentController.confirmPayment);
 
 router.use(auth(Role.CUSTOMER));
 
